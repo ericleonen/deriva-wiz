293403c1b2106bd549ef273a922478b439dc15f8
ericleonen <ericleonen@gmail.com>
2024-11-22 19:09:32 -0800

    add descriptive comments

--- src/lib/symbo-diff/Function.ts
+++ src/lib/symbo-diff/Function.ts
@@ -2,9 +2,27 @@
  * A differentiable univariate function. 
  */
 export default abstract class Function {
+    // TODO: Decide a way to store the domain of a function
+
+    /**
+     * Evaluates this function at the given number and returns the output. Returns undefined if the
+     * given number is outside of this function's domain.
+     */
     public abstract eval(x: number): number | undefined;
+
+    /**
+     * Returns the derivative of this Function.
+     */
     public abstract get derivative(): Function; // TODO: Decide if memoization is needed
+
+    /**
+     * Returns the a simple version of this Function.
+     */
     public abstract get simplified(): Function; // TODO: Decide if memoization is needed
+
+    /**
+     * Returns this function as a Latex expression
+     */
     public abstract get latex(): string; // TODO: Decide if memoization is needed
 
     /**
