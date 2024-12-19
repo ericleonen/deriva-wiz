569c4c2c69331fa6797d7a414026056363831fe8
ericleonen <ericleonen@gmail.com>
2024-12-18 20:49:51 -0800

    change title

--- src/app/play/[difficulty]/layout.tsx
+++ src/app/play/[difficulty]/layout.tsx
@@ -1,11 +1,8 @@
-import { capitalize } from "./utils";
+import { Metadata } from "next";
 
-// @ts-ignore
-export async function generateMetadata({ params }) {
-    return {
-        title: `Deriva-Wiz ${capitalize(params.difficulty)} Mode`
-    };
-}
+export const metadata: Metadata = {
+    title: "Play Deriva-Wiz"
+};
 
 export default function PlayLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return children;
